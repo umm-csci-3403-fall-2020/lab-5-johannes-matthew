08b4dd6f7199cd2f65f29d7c89476183c18fd515
Johannes Martinez <mart4695@morris.umn.edu>
2020-10-14 14:45:19 -0500

    Print messages to the user if there is a networking or io error

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -48,4 +48,13 @@ public class EchoClient {
     input.close();
     output.close();
 
-
+    // Provide some minimal error handling
+  } catch (ConnectException ce) {
+    System.out.println("We were unable to connect to " + server);
+    System.out.println("You should make sure the server is running.");
+} catch (IOException ioe) {
+    System.out.println("We caught an unexpected exception");
+    System.err.println(ioe);
+}
+}
+}

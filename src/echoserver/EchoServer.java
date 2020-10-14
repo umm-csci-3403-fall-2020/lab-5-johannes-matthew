7635a4dbb4ba9276b35d24c0de9da93557441790
Johannes Martinez <mart4695@morris.umn.edu>
2020-10-14 15:01:11 -0500

    Import server starter code (details in description)
    We create a server that responds to requests received through port 6013. We use a socket to receive and send data through this port. We run in an infinite while loop, looking for requests. If there is a basic io error, the user will be notified.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,27 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+    public static final int portNumber = 6013;
+    public static void main(String[] args) {
+        try {
+          // Start listening on the specified port
+          ServerSocket sock = new ServerSocket(portNumber);
+    
+          // Run forever, which is common for server style services
+          while (true) {
+            // Wait until someone connects, thereby requesting a date
+            Socket client = sock.accept();
+            System.out.println("Got a request!");
+    
+          }
+          
+        // *Very* minimal error handling.
+        } catch (IOException ioe) {
+          System.out.println("We caught an unexpected exception");
+          System.err.println(ioe);
+        }
+    }
 }

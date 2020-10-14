98706c1d2ca65ec6bb89260581a6c36c8ebcea0c
Johannes Martinez <mart4695@morris.umn.edu>
2020-10-14 14:40:00 -0500

    Import code from the date server example (details in description)
    We use the java.net package to build a network socket for our client. If given a server name as an argument, our server gets named accordingly. We use a free port number (6013).

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,21 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoClient {
-}
+    public static final int portNumber = 6013;
+    public static void main(String[] args) throws IOException {
+        String server;
+        // Use "127.0.0.1", i.e., localhost, if no server is specified.
+        if (args.length == 0) {
+          server = "127.0.0.1";
+        } else {
+          server = args[0];
+        }
+    
+    try {
+  
+    Socket socket = new Socket(server, portNumber); // Connect to the server
+
+  
